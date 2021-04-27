f757a46b77f7041930fec7c43e09e3791ec1e284
jcolom <jcolom@mdata360.com>
2021-04-27 15:56:27 +0200

    crear / obtener comanda y crear linea de comanda

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-const mssql = require('mssql')
 const db = require('./database')
+var cors = require('cors');
+const { response } = require('express');
+
+app.use(cors({ origin: '*' , credentials :  true}));
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
@@ -9,27 +14,45 @@ app.get('/', (req, res) => {
 })
 
 // Obtiene las bebidas que saldran el la pantalla que estan en stock
-app.get('/GetBebidaMenu', async (req,res)=>{
+app.get('/getBebidaMenu', async (req,res)=>{
     let result = await db.GetBebidasMenu();
     res.send(result);
 })
 
 // Obtiene solo los refrescos que estan en stock
-app.get('/GetBebidasRefrescos', async (req,res)=>{
+app.get('/getBebidasRefrescos', async (req,res)=>{
     let result = await db.GetBebidasRefrescos();
     res.send(result);
 })
 
 // Obtiene solo los alcoholes destilados que estan en stock
-app.get('/GetBebidasAlcoholDestilado', async (req,res)=>{
+app.get('/getBebidasAlcoholDestilado', async (req,res)=>{
     let result = await db.GetBebidasAlcoholDestilado();
     res.send(result);
 })
 
 // Obtiene toda la tabla de bebidas
-app.get('/GetTodasBebidas', async (req,res)=>{
+app.get('/getTodasBebidas', async (req,res)=>{
     let result = await db.GetBebidasAlcoholDestilado();
     res.send(result);
+})
+
+app.get('/getComandaRellenando', async (req,res)=>{
+    
+    let mesa = req.query.mesa
+    let result = await db.GetComandaRellenando(mesa);
+    res.send(result)
+})
+
+app.post('/createComanda',async (req,res)=>{
+    console.log(req.body)
+    let result = await db.CreateComanda(req.body)
+    res.send(result)
+})
+
+app.post('/createLineaComanda',async (req,res)=>{
+    let result = await db.CreateLienaComanda(req.body)
+    res.send(result)
 })
 
 app.listen(3000, (err) => {
--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mssql = require('mssql')
 // Configuracion de la base de datos / conexion
 const dbConfig = {
     user: 'masteruser',
-    password: '',
+    password: 'Parcou11.',
     server: 'juliatest.database.windows.net',
     database: 'JuliaTest',
     requestTimeout: 60000
@@ -27,11 +27,35 @@ async function GetTodasBebidas(){
     let result = await execSQLWithinPool("SELECT Id,Nombre,[Type],Precio,Img FROM [dbo].[Bebidas] ")
     return result;
 }
+// Esta funcion solo devuelve la comanda que se esta rellenando
+async function GetComandaRellenando(mesa){
+    // Estados:
+    //0 - Rellenando comanda
+    //1 - Comanda enviada para procesado
+    //2 - Comanda pagada pendiente de servir
+    //3 - Comanda servida
+    let result = await execSQLWithinPool("  SELECT * FROM Comanda WHERE Estado = 0 and Mesa = "+mesa)
+    return result
+}
+
+async function CreateComanda(comanda){
+
+    let result = await execSQLWithinPool("INSERT INTO [dbo].[Comanda] ([Mesa],[Estado],[Hora]) OUTPUT INSERTED.Id VALUES ("+ comanda.Mesa +"," + comanda.Estado + ",'" + comanda.Hora + "')")
+    
+    return result
+}
+
+async function CreateLienaComanda(lineaComanda){
+
+    let result = await execSQLWithinPool("INSERT INTO [dbo].[Linea_Comanda] ([Id_Comanda],[Id_Comida],[Id_Bebida],[Comentario],[Cantidad]) OUTPUT INSERTED.Id VALUES ("+ lineaComanda.idComanda +","+ lineaComanda.idComida +","+ lineaComanda.idBebida +",'"+ lineaComanda.Comentario +"',"+ lineaComanda.cantidad +")")
+
+    return result
+}
 
 
 // Ejecuta las instrucciones sql que le mandamos
 async function execSQLWithinPool(sqlquery) {
-    pool = new mssql.ConnectionPool(dbConfig);
+    var pool = new mssql.ConnectionPool(dbConfig);
     pool.on('error', err => {
         console.log('SQL errors ', err);
     })
@@ -49,4 +73,4 @@ async function execSQLWithinPool(sqlquery) {
 
 }
 
-module.exports = {GetTodasBebidas,GetBebidasRefrescos,GetBebidasAlcoholDestilado,GetBebidasMenu}
+module.exports = {GetTodasBebidas,GetBebidasRefrescos,GetBebidasAlcoholDestilado,GetBebidasMenu,GetComandaRellenando,CreateComanda,CreateLienaComanda}
